613e0863c41ccce50a6218bcfd721d0317ab5e25
Yairyesidp <yairyesidp@gmail.com>
2024-07-10 15:25:49 -0500

    Add files via upload

---
+++ Connection.php
@@ -0,0 +1,9 @@
+<?php
+	$Host = "localhost";
+	$Username = "id22414550_user";
+	$Password = "Manuelpa97@";
+	$Db = "id22414550_entregas";
+	
+	$Conn = mysqli_connect($Host,$Username,$Password,$Db);
+
+?>
